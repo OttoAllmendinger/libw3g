7a897fb36e895abd292aa50050b4ce5197bd997d
otto.allmendinger@googlemail.com <none@none>
2009-11-26 21:11:28 +0100

    - added overview  - moved directories around
    --HG--
    rename : src/replayuploader/Application.py => ReplayUploader/Application.py
    rename : src/replayuploader/Config.py => ReplayUploader/Config.py
    rename : src/replayuploader/MultipartPostHandler.py => ReplayUploader/MultipartPostHandler.py
    rename : src/replayuploader/ReplayUploader.py => ReplayUploader/ReplayUploader.py
    rename : src/replayuploader/trayicon.gif => ReplayUploader/trayicon.gif
    rename : src/dotacgi/DotaUnits.py => dotastats/DotaUnits.py
    rename : src/dotacgi/colorscheme.txt => dotastats/colorscheme.txt
    rename : src/dotacgi/dotacgi.py => dotastats/dotacgi.py
    rename : src/dotacgi/dump_replay.py => dotastats/dump_replay.py
    rename : src/dotacgi/libw3g => dotastats/libw3g
    rename : src/dotacgi/lighttpd.conf => dotastats/lighttpd.conf
    rename : src/dotacgi/local-replays => dotastats/local-replays
    rename : src/dotacgi/parseDota.py => dotastats/parseDota.py
    rename : src/dotacgi/show-replay.sh => dotastats/show-replay.sh
    rename : src/dotacgi/test.py => dotastats/test.py
    rename : src/dotacgi/test_parser.py => dotastats/test_parser.py
    rename : src/dotacgi/units-6.60.json => dotastats/units-6.60.json
    rename : src/dotacgi/upload.py => dotastats/upload.py
    rename : src/dotacgi/webserver.py => dotastats/webserver.py
    rename : src/libw3g/ActionBlockReader.py => libw3g/ActionBlockReader.py
    rename : src/libw3g/BlockReader.py => libw3g/BlockReader.py
    rename : src/libw3g/Debug.py => libw3g/Debug.py
    rename : src/libw3g/GameBlockReader.py => libw3g/GameBlockReader.py
    rename : src/libw3g/ReplayReader.py => libw3g/ReplayReader.py
    rename : src/libw3g/Tools.py => libw3g/Tools.py
    rename : src/libw3g/__init__.py => libw3g/__init__.py

--- dotastats/webserver.py
+++ dotastats/webserver.py
@@ -1,45 +1,85 @@
 import os
-import os.path as P
+from os.path import join, dirname, exists
 import sys
 import hashlib
 from cStringIO import StringIO
 import time
 import json
 import pprint
-
 from collections import defaultdict
 
 import cherrypy
+from genshi.template import TemplateLoader
 
+import libw3g.Tools
 import parseDota
 import DotaUnits
 
 
-REPLAY_DIR = P.join(P.dirname(__file__), 'replays')
-DATA_DIR = P.join(P.dirname(__file__), 'data')
+REPLAY_DIR = join(dirname(__file__), 'replays')
+DATA_DIR = join(dirname(__file__), 'data')
 
 DEBUG_FAKE_UPLOAD_LAG = False
 
-def get_metadata(replay_dir):
-    return json.load(file(P.join(REPLAY_DIR, replay_dir, 'fileinfo.json')))
+loader = TemplateLoader(join(dirname(__file__), 'html'), auto_reload=True)
 
-def get_replaydata(replay_dir):
-    return json.load(file(P.join(REPLAY_DIR, replay_dir, 'replay.json')))
+def get_metadata(replay_dir):
+    return json.load(file(join(REPLAY_DIR, replay_dir, 'fileinfo.json')))
+
+def get_dota_stats(replay_dir):
+    return json.load(file(join(REPLAY_DIR, replay_dir, 'dota_stats.json')))
 
 def get_replay_hashs():
-    return os.listdir(P.join(REPLAY_DIR))
+    return os.listdir(join(REPLAY_DIR))
 
 def get_replay_dir(replay_file_hash):
-    return P.join(REPLAY_DIR, replay_file_hash)
+    return join(REPLAY_DIR, replay_file_hash)
 
 def replay_exists(replay_file_hash):
-    return P.exists(get_replay_dir(replay_file_hash))
+    return exists(get_replay_dir(replay_file_hash))
 
 def get_hero_name(hero_id):
     return DotaUnits.getUnit(unicode(hero_id).encode('utf8')).getName()
 
 def is_valid_replay_hash(replay_hash):
     return all((c in '0123456789abcdef') for c in replay_hash)
+
+def save_replay(replay_hash, io):
+    assert is_valid_replay_hash(replay_hash)
+    replay_dir = get_replay_dir(replay_hash)
+    if not exists(replay_dir):
+        os.mkdir(replay_dir)
+    file(join(replay_dir, 'replay.w3g'), 'w').write(io.read())
+
+def save_metadata(replay_hash, metadata):
+    assert is_valid_replay_hash(replay_hash)
+    replay_dir = get_replay_dir(replay_hash)
+    metadata_file = file(join(replay_dir, 'fileinfo.json'), 'w')
+    json.dump(metadata, metadata_file)
+
+def save_dota_stats(replay_hash, dota_stats):
+    assert is_valid_replay_hash(replay_hash)
+    replay_dir = get_replay_dir(replay_hash)
+    dota_stats_file = file(join(replay_dir, 'dota_stats.json'), 'w')
+    json.dump(dota_stats, dota_stats_file)
+
+def is_dota_game(replay_hash):
+    return get_metadata(replay_hash)['is_dota_game']
+
+def get_dota_games():
+    return map(get_dota_stats, filter(is_dota_game, get_replay_hashs()))
+
+def get_play_days():
+    #TODO: do this
+    for game in get_dota_games():
+        timestamp = game['timestamp']
+
+def get_players():
+    players = set()
+    for game in get_dota_games():
+        players |= set(p['Name'] for p in game['Slots'].values())
+    return players
+
 
 class DotaStats:
     def exists(self, replay_file_hash):
@@ -62,22 +102,22 @@ class DotaStats:
     @cherrypy.expose
     def dumpjson(self, replay_hash):
         assert is_valid_replay_hash(replay_hash)
-        gamedata = get_replaydata(replay_hash)
+        dota_stats = get_dota_stats(replay_hash)
         return ('<plaintext>'+
-                pprint.pformat(gamedata))
+                pprint.pformat(dota_stats))
 
     @cherrypy.expose
     def reparse(self, replay_hash):
         assert is_valid_replay_hash(replay_hash)
-        fileio = file(P.join(get_replay_dir(replay_hash), 'replay.w3g'), 'r')
-        self.parse(fileio, overwrite=True)
+        fileio = file(join(get_replay_dir(replay_hash), 'replay.w3g'), 'r')
+        save_dota_stats(replay_hash, parseDota.parseDotaReplay(fileio))
         return self.dumpjson(replay_hash)
 
     @cherrypy.expose
     def reparse_all(self):
         for replay_hash in get_replay_hashs():
-            fileio = file(P.join(get_replay_dir(replay_hash), 'replay.w3g'), 'r')
-            self.parse(fileio, overwrite=True)
+            fileio = file(join(get_replay_dir(replay_hash), 'replay.w3g'), 'r')
+            save_dota_stats(replay_hash, parseDota.parseDotaReplay(fileio))
         return 'done'
 
     @cherrypy.expose
@@ -88,8 +128,10 @@ class DotaStats:
 
         for replay_hash in get_replay_hashs():
             metadata = get_metadata(replay_hash)
-            gamedata = get_replaydata(replay_hash)
-            players = [s.get('Name') for s in gamedata['Slots'].values()]
+            if not metadata['is_dota_game']:
+                continue
+            dota_stats = get_dota_stats(replay_hash)
+            players = [s.get('Name') for s in dota_stats['Slots'].values()]
             buf.append(u'<li><a href="/replay/%s">%s (%s)</a>' % (replay_hash,
                 time.asctime(time.localtime(metadata['file_timestamp'])),
                 u', '.join(players)))
@@ -100,6 +142,11 @@ class DotaStats:
         buf.append('</ul></body></html>')
         return u''.join(buf).encode('utf8')
 
+    @cherrypy.expose
+    def overview(self):
+        return loader.load('overview.tpl.html').generate(
+                play_days=get_play_days(), players=get_players()).render('html')
+
 
     @cherrypy.expose
     def replay(self, replay_hash):
@@ -107,7 +154,7 @@ class DotaStats:
         if not replay_exists(replay_hash):
             return 'REPLAY_NOT_FOUND'
 
-        gamedata = get_replaydata(replay_hash)
+        dota_stats = get_dota_stats(replay_hash)
         buf = [('<html><body><table>'
                 '<tr>'
                 '<th>Name</th>'
@@ -116,7 +163,7 @@ class DotaStats:
                 '<th>Deaths</th>'
                 '<th>Assists</th>'
                 '</tr>')]
-        for slot in gamedata['Slots'].values():
+        for slot in dota_stats['Slots'].values():
             stats = slot.get('Stats', {})
             buf.append(
                     '<tr>' +
@@ -152,38 +199,29 @@ class DotaStats:
                 time.sleep(0.1)
         yield '-'*8
 
-        replay_file_hash = self.parse(tmpio)
+        tmpio.seek(0)
+        replay_hash = hashlib.md5(tmpio.read()).hexdigest()
+        tmpio.seek(0)
+        save_replay(replay_hash, tmpio)
         metadata = {
             'file_timestamp': int(timestamp),
             'upload_timestamp': int(time.time()),
             'uploader_ip': cherrypy.request.remote.ip }
-        self.save_metadata(replay_file_hash, metadata)
+        try:
+            tmpio.seek(0)
+            dota_stats = parseDota.parseDotaReplay(tmpio)
+            save_dota_stats(replay_hash, dota_stats)
+            metadata['is_dota_game'] = True
+            save_metadata(replay_hash, metadata)
+        except:
+            metadata['is_dota_game'] = False
+            save_metadata(replay_hash, metadata)
+            raise
 
-    def parse(self, replayio, overwrite=False):
-        replayio.seek(0)
-        state = parseDota.parseDotaReplay(replayio)
-        replayio.seek(0)
-        replay_file_hash = hashlib.md5(replayio.read()).hexdigest()
-        if (not replay_exists(replay_file_hash)) or overwrite:
-            replay_dir = get_replay_dir(replay_file_hash)
-            if not P.exists(replay_dir):
-                os.mkdir(replay_dir)
-            uploaded_replay_file = file(P.join(replay_dir, 'replay.w3g'), 'w')
-            replayio.seek(0)
-            uploaded_replay_file.write(replayio.read())
-            replay_state = file(P.join(replay_dir, 'replay.json'), 'w')
-            json.dump(state, replay_state)
-        return replay_file_hash
-
-    def save_metadata(self, replay_hash, metadata):
-        assert is_valid_replay_hash(replay_hash)
-        replay_dir = get_replay_dir(replay_hash)
-        metadata_file = file(P.join(replay_dir, 'fileinfo.json'), 'w')
-        json.dump(metadata, metadata_file)
 
     @cherrypy.expose
     def highscore(self):
-        all_replay_data = [get_replaydata(replay_hash) for replay_hash in get_replay_hashs()]
+        all_replay_data = [get_dota_stats(replay_hash) for replay_hash in get_replay_hashs()]
         kills = defaultdict(int)
         for replay_data in all_replay_data:
             for slot in replay_data['Slots'].values():
